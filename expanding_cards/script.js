41f0e9ecaca31bc45938e64d1dee683b14ecf079
Yooboo Park <yooboopark@Yooboos-Air.home>
2022-03-15 16:48:01 -0500

    let to const

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,9 +1,9 @@
 const panels = document.querySelectorAll('.panel')
-console.log(panels)
+
+
 panels.forEach(panel => {
   panel.addEventListener('click', () => {
     const activePanel = document.getElementsByClassName('panel active')
-
     activePanel[0].classList.remove('active')
     panel.classList.add('active')
   })
